048228fc371922231050a334ef344058ef3a06f5
Paine12 <38664927+Paine12@users.noreply.github.com>
2024-02-05 17:46:10 +0200

    Finish grid and coloring with black

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 let container = document.querySelector(".container")
+let button = document.querySelector(".clear")
+
+
+var mouseDown = 0;
+document.body.onmousedown = function() { 
+  mouseDown=1;
+}
+document.body.onmouseup = function() {
+  mouseDown=0;
+}
 
 function createDivs(){
     for(i=1;i<=16;i++){
         for(j=1;j<=16;j++){
             let newdiv = document.createElement("div")
-            newdiv.setAttribute("style", "width: 30px; height: 30px; display: inline-block; background-color: blue; margin:5px")
+            newdiv.setAttribute("style", "background-color: white;")
+
+            newdiv.addEventListener("mouseover", () => {
+                if(mouseDown){
+                    newdiv.setAttribute("style", "background-color: black;")
+                }
+                
+            })
             container.appendChild(newdiv);
+            
         }
     }
 }
 
+function deleteDivs(){
+    let children = container.children;
+    children.forEach(child => {
+        container.removeChild(child);
+    });
+}
+
 createDivs();
+
+button.addEventListener("click", () => {
+    deleteDivs;
+    createDivs;
+})
